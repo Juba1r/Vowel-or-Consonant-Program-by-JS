d0b49327d07bcab270f30f34c4b66c358ef1cf04
Jubair Ibn Khaled <160339037+Pro-jubair@users.noreply.github.com>
2024-02-19 15:46:50 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var letter = prompt("Enter the letter");
+
+letter.toLowerCase();
+
+if(letter=="a" || letter=="e" || letter=="i" || letter=="o" || letter=="u")
+console.log("vowel");
+
+else
+console.log("consonant");
